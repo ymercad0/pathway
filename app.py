aa41eeee5bc783b077eedf27edc13eb8280a712f
XV1R <xavytron@gmail.com>
2022-04-11 23:04:36 -0400

    Working horizontal scroll in index of recent reviews; removed extra __init__.py; added placeholder company data for modeling index

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import re
 from flask import Flask, url_for, render_template
+import model
 
 
 app = Flask(__name__) 
@@ -8,4 +10,21 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index") 
 def index():
-    return render_template("index.html")
+    company1 = model.Company(
+			name="Microsoft",
+			category="software",
+			logo_img="https://bit.ly/3uWfYzK",
+			banner_img="https://bit.ly/3xfolJs"
+			)
+    review_1 = model.Review(
+            company1,
+            "Placeholder Review",
+            "Security Engineering",
+            "Security Engineer Intern",
+            4,
+            "B.S.",
+            "Had a good time overall. Tasking was tough and hours were long.",
+            5
+            )
+    placeholder = [review_1 for _ in range(10)]
+    return render_template("index.html", recent_reviews=placeholder)
