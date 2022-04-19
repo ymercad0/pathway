b3b5a7a5e6733d6fd5b3271dd0a095f342888f7e
XV1R <xavytron@gmail.com>
2022-04-18 23:51:14 -0400

    user route with some placeholder values

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ db = mongo.db
 for collection in model.collections:
     if collection not in db.list_collection_names():
         db.create_collection(collection)
-
         if collection == "companies":
             # add backup companies in case we reset
             # a collection
@@ -105,11 +104,41 @@ def user():
     """Route for user's profile page with information and account controls. 
     If the user is not logged in, redirect to login page? 
     """
+    if 'username' not in session:
+        return redirect(url_for("login"))
+    company1 = model.Company(
+			name="Microsoft",
+			category="Software",
+			logo_img="https://bit.ly/3uWfYzK",
+			banner_img="https://bit.ly/3xfolJs"
+			)
+    company2 = model.Company(
+			name="Google",
+			category="Software",
+			logo_img="https://bit.ly/3Jvmy5t",
+			banner_img="http://somelink.com"
+			)
+    review_1 = model.Review('user',company1,"Placeholder Review","Security Engineering",
+        "Security Engineer Intern",company_rating=4,education="B.S.",
+        interview_desc="Had a good time overall. Tasking was tough and hours were long.",
+        interview_rat=5,offer=True, accepted=False, start_date="05-23-2022",
+        intern_desc="desc", work_rat=None, culture_rat=None, location=("San Francisco", "California"),
+        pay=35.25, bonuses="Bonus")
+
+    review_2 = model.Review('user',company2, "Title", 'Software Engineering',
+        "Position", company_rating=4, education="M.S.", interview_desc="I did this x y z dsdsasddddddddddddddddddd",
+        interview_rat=4, offer=False, accepted=False, start_date="05-23-2022",
+        intern_desc="desc", work_rat=None, culture_rat=None, location=("San Francisco", "California"),
+        pay=35.25, bonuses="Bonus")
+
+
+    placeholder = [review_1 for _ in range(3)]
+    placeholder.extend([review_2 for _ in range(3)])
     users = db.users
     reviews = db.reviews
     user = users.find_one({"username":session["username"]})
-    user_reviews = [rev for rev in reviews.find({"user":user['username']})]
-    return render_template('user.html',user=user, reviews=user_reviews)
+    # user_reviews = [rev for rev in reviews.find({"user":user['username']})]
+    return render_template('user.html',user=user, reviews=placeholder)
 
 
 @app.route("/")
