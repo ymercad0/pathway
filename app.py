c96a098c5c27f43480d96b3b03272a305ee6950c
XV1R <xavytron@gmail.com>
2022-04-20 00:49:55 -0400

    review page skeleton; small changes in other files

--- app.py
+++ app.py
@@ -176,6 +176,39 @@ def change_pfp(username):
     else:
         return "Not the user!"
 
+@app.route("/reviews", methods=["GET"])
+def reviews():
+    company1 = model.Company(
+			name="Microsoft",
+			category="Software",
+			logo_img="https://bit.ly/3uWfYzK",
+			banner_img="https://bit.ly/3xfolJs"
+			)
+    company2 = model.Company(
+			name="Google",
+			category="Software",
+			logo_img="https://bit.ly/3Jvmy5t",
+			banner_img="http://somelink.com"
+			)
+    review_1 = model.Review('user',company1,"Placeholder Review","Security Engineering",
+        "Security Engineer Intern",company_rating=4,education="B.S.",
+        interview_desc="Had a good time overall. Tasking was tough and hours were long.",
+        interview_rat=5,offer=True, accepted=True, start_date="05-23-2022",
+        intern_desc="desc", work_rat=None, culture_rat=None, location=("San Francisco", "California"),
+        pay=35.25, bonuses="Bonus")
+
+    review_2 = model.Review('user',company2, "Title", 'Software Engineering',
+        "Position", company_rating=4, education="M.S.", interview_desc="I did this x y z dsdsasddddddddddddddddddd",
+        interview_rat=4, offer=False, accepted=False, start_date="05-23-2022",
+        intern_desc="desc", work_rat=None, culture_rat=None, location=("San Francisco", "California"),
+        pay=35.25, bonuses="Bonus")
+
+
+    placeholder = [review_1 for _ in range(3)]
+    placeholder.extend([review_2 for _ in range(3)])
+    # rev = db.reviews.find({})
+    return render_template("reviews.html",reviews=placeholder)
+
 
 @app.route("/")
 @app.route("/index")
